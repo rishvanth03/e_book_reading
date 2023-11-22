46dde4164f1dad8af0e73adeecb5d6ba54fa5c8e
Rishvanth Rajaa NL <rishvanthrajaa.cs21@bitsathy.ac.in>
2023-11-22 16:16:08 +0530

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:e_book_reader/pages/root_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
+
